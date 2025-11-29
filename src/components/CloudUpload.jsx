a45138a9448e38822c372a03f61084ace67c7bc3
Amangeldi96 <193253467+Amangeldi96@users.noreply.github.com>
2025-11-29 20:15:30 +0600

    fix firebase

--- src/components/CloudUpload.jsx
+++ src/components/CloudUpload.jsx
@@ -3,20 +3,30 @@ import React, { useState } from "react";
 export default function CloudUpload({ onUpload }) {
   const [preview, setPreview] = useState("");
 
-  async function uploadToCloudinary(file) {
+  const uploadToCloudinary = async (file) => {
     const data = new FormData();
     data.append("file", file);
-    data.append("upload_preset", "toktogul"); // твой preset
+    data.append("upload_preset", "toktogul"); 
     data.append("cloud_name", "dqzgtlvlu");
 
     const res = await fetch(
       "https://api.cloudinary.com/v1_1/dqzgtlvlu/image/upload",
-      { method: "POST", body: data }
+      {
+        method: "POST",
+        body: data,
+      }
     );
 
     const json = await res.json();
+
+    if (!json.secure_url) {
+      console.log("Ошибка Cloudinary:", json);
+      alert("Ошибка загрузки файла в Cloudinary");
+      return null;
+    }
+
     return json.secure_url;
-  }
+  };
 
   const handleChange = async (e) => {
     const file = e.target.files[0];
@@ -26,7 +36,9 @@ export default function CloudUpload({ onUpload }) {
 
     const url = await uploadToCloudinary(file);
 
-    if (onUpload) onUpload(url);
+    if (url && onUpload) {
+      onUpload(url);
+    }
   };
 
   return (
