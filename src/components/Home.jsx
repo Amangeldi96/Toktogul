d055d7c478de53e3a5082b27d033f8707ead1a17
Amangeldi96 <193253467+Amangeldi96@users.noreply.github.com>
2025-12-07 21:52:12 +0600

    fix firebase

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -165,7 +165,7 @@ export default function Home() {
 
   // ===== Labels и категории =====
   const addressLabels = {
-    toktogul: "Токтгул",
+    toktogul: "Токтогул",
     janyjol: "Жаңы-Жол",
     uchterek: "Үч-Терек",
     tereksuu: "Терек-Суу",
